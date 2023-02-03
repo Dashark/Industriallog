fe31054a3c6324079ab1d3d65ce22ab5e368fdb7
Lake.Hu <hudasha@scu.edu.cn>
2023-02-03 20:46:44 +0800

    GDF数据上传

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # 这里是添加运行那个文件的配置
 python /opt/Industriallog/Deal_name_mtr.py
-echo "hello world" >> /opt/Industriallog/log.txt
+python /opt/Industriallog/Deal_name_gdf.py
+# echo "hello world" >> /opt/Industriallog/log.txt
 # 系统运行一直
 # 更新的
